d1771fd8da4b0959df310883727e65683001a6bf
SUPRIYAKUKUNURU <supriyakukunuru2001@gmail.com>
2025-09-17 17:11:50 +0530

    first commit

---
+++ conditional-rendering/src/App.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+function App(){
+  // if-else:
+          // const stock = "true";
+          // if(stock)
+          // return<button className="bg-success">Stock Available</button>
+          // else
+          //   return<button className="bg-danger">Stock Unavailable</button>
+
+  // else-if:
+
+          //   const stock =0;
+          //   let components=true;
+          //   if(stock)
+          //     components=<button className="bg-success">Available</button>
+          //     else
+          //       components=<button className="bg-danger">Out of Stock</button>
+
+          //     return(
+          //       <>
+          //       {components}
+          //       </>
+          //     )
+
+//ternary operator
+
+
+  const stock = false;
+  return(
+    <>
+    {stock?<button className="bg-success">Available</button>:<button className="bg-danger">Unavailable</button>}
+    </>
+  )
+
+     }
+
+
+export default App
