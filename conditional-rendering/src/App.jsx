810ee97c54a9a535fa28304785a7ed78e99afbd2
SUPRIYAKUKUNURU <supriyakukunuru2001@gmail.com>
2025-10-05 17:43:14 +0530

    Header added

--- conditional-rendering/src/App.jsx
+++ conditional-rendering/src/App.jsx
@@ -5,13 +5,15 @@ import data from "./components/data.js"
 import Laptop from './components/pages/Laptop'
 import SmartPhones from './components/pages/SmartPhones.jsx'
 import Headphones from './components/pages/Headphones.jsx'
+import Header from './components/Header.jsx'
 function App() {
   return (
     <div>
+      <Header />
       {/* <Conditionals /> */}
       {/* <Products /> */}
       <Laptop data={data} />
-      <SmartPhones data={data}/>
+       <SmartPhones data={data}/>
       <Headphones data={data}/>
     </div>
   )
