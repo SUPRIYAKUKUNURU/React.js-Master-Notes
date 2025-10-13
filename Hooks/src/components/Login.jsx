91f340853e571cc527d792523527a37fd8112e42
SUPRIYAKUKUNURU <supriyakukunuru2001@gmail.com>
2025-10-13 21:56:31 +0530

    first commit

--- Hooks/src/components/Login.jsx
+++ Hooks/src/components/Login.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import "./Login.css"
+import{useState} from 'react';
 function Login() {
+  const [username, setusername] = useState(" ");
+  const [password,setPassword] = useState(" ");
+
+  const handleUsername=(e)=>{
+  setusername(e.target.value)
+  }
+  console.log("Username :" ,username);
+  console.log("Password :",password);
   return (
     <div>
             <form className="form mx-auto  mt-5">
        <p class="form-title">Sign in to your account</p>
         <div class="input-container">
-          <input type="email" placeholder="Enter email"/>
+          <input type="email" placeholder="Enter email" onChange={handleUsername}/>
           <span>
           </span>
       </div>
       <div class="input-container">
-          <input type="password" placeholder="Enter password"/>
+          <input type="password" placeholder="Enter password" onChange={(e)=>setPassword(e.target.value)}/>
         </div>
          <button type="submit" class="submit">
         Sign in
