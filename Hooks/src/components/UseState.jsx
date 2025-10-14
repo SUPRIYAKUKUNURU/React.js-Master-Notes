0758688ac24e173e86d191964601fd358cd8e23a
SUPRIYAKUKUNURU <supriyakukunuru2001@gmail.com>
2025-10-14 13:43:44 +0530

    commit

--- Hooks/src/components/UseState.jsx
+++ Hooks/src/components/UseState.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
+import {useState} from 'react';
 
 function UseState() {
-  return (
-    <div>
-        <h1>Usestate</h1>
-    </div>
+     const [name ,setName] = useState("Priya");
+     const[count,setCount] = useState(0)
+     const [isDark ,setIsDark] = useState(false);
+ 
+  const handleClick=()=>{
+    setName("Supriya Kukunuru");
+  }
+  const handleClick1=()=>{
+   setCount(count+1);
+  }
+  const handleToggle=()=>{
+    setIsDark(!isDark);
+  }
+  
+  return(
+    <>
+    <h1>My Name is {name}</h1>
+    <button onClick={handleClick}>Update Name</button>
+
+    <h1>Counter: {count}</h1>
+    <button onClick={handleClick1}>Click Me</button>
+    
+    <div style={{
+        backgroundColor: isDark ? "black" : "white",
+        color : isDark ? "white" : "black"
+    }}>
+
+  <h2 >{isDark ? "DarkMode" : "Light Mode"}</h2>
+  <button onClick={handleToggle}>{isDark ? "Switch to Light": "Switch to Dark"}</button>
+        </div>
+
+    </>
   )
 }
 
