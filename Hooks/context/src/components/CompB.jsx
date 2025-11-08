6ea22c04214a8e9f9c4b196787bbfc77a7813425
SUPRIYAKUKUNURU <supriyakukunuru2001@gmail.com>
2025-11-08 21:55:37 +0530

    first commit

--- Hooks/context/src/components/CompB.jsx
+++ Hooks/context/src/components/CompB.jsx
@@ -3,11 +3,11 @@ import CompC from './CompC'
 import { courseContext } from './MyContext'
 
 function CompB({name}) {
-    const sub = useContext(courseContext)
+    const subs = useContext(courseContext)
   return (
     <div className='border border-danger p-2'>
         <h2>CompB</h2>
-        <h1>Course:{sub}</h1>
+        <h1>Course:{subs}</h1>
     
         <CompC name = {name} />
     </div>
